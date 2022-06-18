bdc5ea04e7cdd7858e3729b6b8cc95489db832ad
Artur Michałek <sh4ke96@gmail.com>
2022-06-18 13:25:50 +0200

    add icon for navMenu-mobile, RWD for Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -41,6 +41,23 @@
         </button>
       </li>
       <li class="li__icons">
+        <button id="menuIcon" class="menuIcon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
+      </li>
+      <li class="li__icons">
         <button id="dotIcon" class="dotIcon">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -66,6 +83,10 @@ export default {};
 
 <style lang="scss" scoped>
 .headerNav {
+  display: none;
+  @media (min-width: 992px) {
+    display: flex;
+  }
   &__list {
     display: flex;
     list-style: none;
@@ -113,11 +134,20 @@ export default {};
         height: 6rem;
       }
       .bagIcon:hover {
-        background-color:#f5f6fa;
+        background-color: #f5f6fa;
         transition: 0.3s ease-in-out;
       }
       .dotIcon {
+        padding-left: 1rem;
+        @media (min-width: 992px) {
         padding-left: 2rem;
+        }
+      }
+      .menuIcon {
+        padding-left: 1rem;
+        @media (min-width: 992px) {
+          display: none;
+        }
       }
       .bagIcon-number {
         font-style: normal;
@@ -136,7 +166,7 @@ export default {};
       }
     }
     .bi-bag,
-    .bi-three-dots-vertical {
+    .bi-three-dots-vertical, .bi-list {
       width: 2.5rem;
       height: 2.5rem;
     }
